a7c1ca776a5066e9c8c6eaf585a922f8b80d02e5
rakanshaker <shaker.rakan@gmail.com>
2020-01-22 06:43:47 -0500

    Passed DOMstrings to Controller

--- app.js
+++ app.js
@@ -3,25 +3,42 @@ var budgetController = (function() {})();
 
 //UI Controller
 var UIController = (function() {
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    inputButton: ".add__btn"
+  };
+
   return {
     getInput: function() {
-      var type = document.querySelector(".add__type").value; //will be 'inc' or 'exp'
-      var description = document.querySelector(".add__description").value;
-      var value = document.querySelector(".add__value").value;
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, //will be 'inc' or 'exp'
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      };
+    },
+    getDOMstrings: function() {
+      return DOMstrings;
     }
   };
 })();
 
 //Global App Controller
 var controller = (function(budgetCtrl, UICtrl) {
+  var DOM = UIController.getDOMstrings();
   var cntrlAddItem = function() {
     //to do: 1. get the field input data
+    var input = UIController.getInput();
+    console.log(input);
     //2. add item to budget controller
     //3. add the new item to UI
     //4. calculate the budget
     //5. display budget on UI
   };
-  document.querySelector(".add__btn").addEventListener("click", cntrlAddItem);
+  document
+    .querySelector(DOM.inputButton)
+    .addEventListener("click", cntrlAddItem);
 
   document.addEventListener("keypress", function(event) {
     if (event.keyCode === 13 || event.which === 13) {
